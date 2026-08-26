54aaff9e782569c9b27f022f8deaef8b46bd3084
liana-kolodzheieva <liana.kolodgeeva@gmail.com>
2026-08-27 00:25:22 +0200

    sound

--- Js/script.js
+++ Js/script.js
@@ -4,18 +4,35 @@ const audio = document.getElementById("audio");
 const blackLamp = document.getElementById("black_lamp");
 const blackRI = document.getElementById("black_RI");
 const blackOM = document.getElementById("black_OM");
+const startSong = document.getElementById("start_Game");
+const blackOmori = document.getElementsByClassName("black_OMORI");
 
-function startGame() {
-  video.play();
-  video.style.visibility = "visible";
-  startButton.style.visibility = "hidden";
-  audio.src = "songs/BlackSpace.mp3";
-  blackLamp.src = "gif/white_lamp.gif";
-  blackRI.style.visibility = "hidden";
-  blackOM.style.visibility = "hidden";
-  blackLamp.style.opacity = "100%";
+function startMedia() {
+  audio.src = "songs/Title.mp3";
+  audio.loop = true;
+  audio.volume = 0.5;
+  audio.play();
+
+  startSong.style.visibility = "hidden";
+
+  startButton.style.disabled = "false";
+  startButton.classList.add("visible");
+  blackLamp.classList.add("visible");
+  blackOmori[0].classList.add("visible");
+  blackOmori[1].classList.add("visible");
 }
 
-startButton.addEventListener("click", () => {
+function startGame() {
+  video.style.visibility = "visible";
+  video.play();
+
+  audio.src = "songs/BlackSpace.mp3";
+
+  blackRI.style.visibility = "hidden";
+  blackOM.style.visibility = "hidden";
+  startButton.style.visibility = "hidden";
+
+  blackLamp.style.opacity = "100%";
+
   blackLamp.classList.add("move");
-});
+}
